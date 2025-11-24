8ec20659b57b8ca6207a2c268374abef9f5dfce2
abdelilah2005 <abdelilahessemlali@gmail.com>
2025-11-24 14:23:55 +0100

    chore: Remove HTTPS certificate generation and related directory creation from key generation script.

--- generate_keys.sh
+++ generate_keys.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 set -e
 
-mkdir -p secrets certs
+mkdir -p secrets 
 
 echo "🔑 Generating RSA key pair for JWT..."
 openssl genrsa -out secrets/jwt_private.pem 2048
 openssl rsa -in secrets/jwt_private.pem -pubout -out secrets/jwt_public.pem
-
-echo "🔒 Generating self-signed HTTPS certificate..."
-
-
-# command to execute this script in bash terminal
-# bash generate_keys.sh 
